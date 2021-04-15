2d81a02c9adbd206f1ac34a1c4c05bf5bbae50f8
mmergal92 <mmergal92@gmail.com>
2021-04-14 23:04:02 -0400

    set up database

--- gitpub/server.js
+++ gitpub/server.js
@@ -1,14 +1,20 @@
 //DEPENDENCIES
 const express = require ('express');
 const app = express();
+const drinks = require('./models/drinks.js');
+console.log(drinks);
 
 //PORT
 const port = 3000;
 
 //ROUTES
-app.get('/', (req,res) =>{
-    res.send('Welcome to the Github App!');
-  });
+// app.get('/', (req,res) =>{
+//     res.send('Welcome to the Github App!');
+//   });
+
+app.get('/drinks/', (req,res) =>{
+  res.send(drinks);
+})
 
 // LISTENER
 app.listen(port, ()=> {
